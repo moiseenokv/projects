74fa5567c94f4b6979310e8a6711948f7c3904c6
Vitali Maisiayonak <moiseenokv@gmail.com>
2020-03-17 21:29:01 +0300

    add datagrid project

---
+++ datagrid/precache-manifest.d5db91b48a47c009d02ce073eed15cc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35dd6318d7f9bfbee111558e98df4079",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d6f0ccb9b3ee2d077bba",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ead3335e09c3379b4ca3",
+    "url": "./static/css/main.f071e54d.chunk.css"
+  },
+  {
+    "revision": "d6f0ccb9b3ee2d077bba",
+    "url": "./static/js/2.261c0e91.chunk.js"
+  },
+  {
+    "revision": "1ba69035e6edbd45d0f6e0853d2a9b22",
+    "url": "./static/js/2.261c0e91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ead3335e09c3379b4ca3",
+    "url": "./static/js/main.79ef8e75.chunk.js"
+  },
+  {
+    "revision": "15bd71826fd82f232173",
+    "url": "./static/js/runtime-main.f6113c09.js"
+  }
+]);
